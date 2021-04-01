d3170ec1090e50a003316213e4eb81cb7bfb4bfb
yoursemicolon <amandarozikurnia@gmail.com>
2021-04-01 18:35:24 +0700

    Ganti path

--- soal3/soal3b.sh
+++ soal3/soal3b.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # masuk ke directory
-cd /home/yoursemicolon/Documents/sisop-2021/modul-1/soal-shift-modul-1-B05-2021/soal3
+cd ~/soal-shift-sisop-modul-1-B05-2021/soal3
 
 # buat directory baru
-newDir=$(date +"%d+%m+%Y")
+newDir=$(date +"%d-%m-%Y")
 mkdir "$newDir"
 
 # jalankan soal 3a
-bash "./home/yoursemicolon/Documents/sisop-2021/modul-1/soal-shift-modul-1-B05-2021/Soal3/soal3a.sh"
+bash `pwd`/soal3a.sh
+#bash soal3a.sh
 
 # pindahkan semua file ke directory baru
 mv *.jpg "./$newDir"
