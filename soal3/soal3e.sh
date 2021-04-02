a8f7334009ea53a335ca9b6a1630eda6c6ffd9cf
yoursemicolon <amandarozikurnia@gmail.com>
2021-04-02 20:05:05 +0700

    Ubah formate date

--- soal3/soal3e.sh
+++ soal3/soal3e.sh
@@ -4,7 +4,7 @@
 cd /home/yoursemicolon/Documents/sisop-2021/modul-1/soal-shift-modul-1-B05-2021/soal3
 
 # setting password
-setPassword=$(date +"%d%m%Y")
+setPassword=$(date +"%m%d%Y")
 
 # unzip file
 unzip -P "$setPassword" Koleksi.zip
