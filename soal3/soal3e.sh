c4b73d94e9573ebf6f4141e4e1a04a4cedcaa05e
yoursemicolon <amandarozikurnia@gmail.com>
2021-04-22 18:02:47 +0700

    Update path

--- soal3/soal3e.sh
+++ soal3/soal3e.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # masuk ke directory
-cd /home/yoursemicolon/Documents/sisop-2021/modul-1/soal-shift-modul-1-B05-2021/soal3
+cd ~/soal-shift-sisop-modul-1-B05-2021/soal3
 
 # setting password
 setPassword=$(date +"%m%d%Y")
 
 # unzip file
-unzip -P "$setPassword" Koleksi.zip
+unzip -q -P "$setPassword" Koleksi.zip
 
 # hapus file zip
 rm "./Koleksi.zip"
